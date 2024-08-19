7c12ae66bf0eb3453392fef37734872b699975f7
AbayTesfaye <abaytesfaye02@gmail.com>
2024-08-19 16:10:39 -0700

    making active the steps

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,12 +1,20 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 export default function App() {
+  const step = 2;
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">Hello</p>
+      <p className="message">
+        Step{step}:{messages[step - 1]}
+      </p>
       <div className="buttons">
         <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
           Previous
