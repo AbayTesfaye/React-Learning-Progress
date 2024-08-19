dc30685101be16b0b05085145829dbda0e81a851
AbayTesfaye <abaytesfaye02@gmail.com>
2024-08-18 18:27:38 -0700

    start new project and this is the structure of the project

---
+++ steps/src/App.js
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
