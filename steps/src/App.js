a010cedc6b69671f50ffd5fd85174a2648d38fb3
AbayTesfaye <abaytesfaye02@gmail.com>
2024-08-19 18:02:43 -0700

    Using current value to update the state variable

--- steps/src/App.js
+++ steps/src/App.js
@@ -9,14 +9,14 @@ export default function App() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen && (
