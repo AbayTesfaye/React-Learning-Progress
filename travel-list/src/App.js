d60e56b6ac6aa732f048cc758583260d743f4e51
AbayTesfaye <abaytesfaye02@gmail.com>
2024-08-22 15:31:53 -0700

    Structure for Far Away mini-project

---
+++ travel-list/src/App.js
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌳 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your list, and You're already packed X(x%)</em>
+    </footer>
+  );
+}
