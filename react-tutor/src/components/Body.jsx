bbf34d4d6b3b1382827927cd4204271b5f2e966e
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-13 08:54:11 -0700

    Day 2: About Componenet

---
+++ react-tutor/src/components/Body.jsx
@@ -0,0 +1,9 @@
+export function Body() {
+  return (
+    <ul>
+      <li>Burger</li>
+      <li>Pizza</li>
+      <li>Pasta</li>
+    </ul>
+  );
+}
---
+++ react-tutor/src/components/Heading.jsx
@@ -0,0 +1,4 @@
+function Heading() {
+  return <h1>My Favourite Food</h1>;
+}
+export default Heading;
